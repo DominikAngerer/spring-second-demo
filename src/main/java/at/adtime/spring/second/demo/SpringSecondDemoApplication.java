e14fd0dcf14e49f38734181969889f164d06a2b5
Dominik Angerer <git@adtime.at>
2015-01-22 22:45:24 +0100

    Created web-spring-project using STS

---
+++ src/main/java/at/adtime/spring/second/demo/SpringSecondDemoApplication.java
@@ -0,0 +1,12 @@
+package at.adtime.spring.second.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecondDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/at/adtime/spring/second/demo/SpringSecondDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package at.adtime.spring.second.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecondDemoApplication.class)
+@WebAppConfiguration
+public class SpringSecondDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
