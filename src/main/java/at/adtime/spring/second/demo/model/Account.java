f52f47d865fc17da2af18f9debf65b88ef9d50cd
Dominik Angerer <git@adtime.at>
2015-01-22 22:58:50 +0100

    Added Account Entity Added Bookmark Entity

---
+++ src/main/java/at/adtime/spring/second/demo/model/Account.java
@@ -0,0 +1,50 @@
+package at.adtime.spring.second.demo.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class Account {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@JsonIgnore
+	public String password;
+	public String username;
+
+	@OneToMany(mappedBy = "account")
+	private Set<Bookmark> bookmarks = new HashSet<>();
+
+	public Account(String name, String password) {
+		this.username = name;
+		this.password = password;
+	}
+
+	Account() { // jpa only
+	}
+
+	public Set<Bookmark> getBookmarks() {
+		return bookmarks;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+}
---
+++ src/main/java/at/adtime/spring/second/demo/model/Bookmark.java
@@ -0,0 +1,9 @@
+package at.adtime.spring.second.demo.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Bookmark {
+
+	
+}
