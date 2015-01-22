6a4e0d816c10bbcdb6d43bbf00dbfcf7eb8d1e1b
Dominik Angerer <git@adtime.at>
2015-01-22 22:59:08 +0100

    Added Properties to Bookmark Entity

--- src/main/java/at/adtime/spring/second/demo/model/Bookmark.java
+++ src/main/java/at/adtime/spring/second/demo/model/Bookmark.java
@@ -1,9 +1,48 @@
 package at.adtime.spring.second.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Bookmark {
 
-	
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@JsonIgnore
+	@ManyToOne
+	private Account account;
+
+	public String uri;
+	public String description;
+
+	public Bookmark(Account account, String uri, String description) {
+		this.uri = uri;
+		this.description = description;
+		this.account = account;
+	}
+
+	Bookmark() { // jpa only
+	}
+
+	public Account getAccount() {
+		return account;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 }
