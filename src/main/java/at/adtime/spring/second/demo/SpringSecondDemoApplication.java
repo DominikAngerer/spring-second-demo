8b7004044e8758c95596e5ba7c165ebc0cf0eaab
Dominik Angerer <git@adtime.at>
2015-01-22 23:13:21 +0100

    Changed to Java RE 1.8 so we can use functionality from it ;)

--- src/main/java/at/adtime/spring/second/demo/SpringSecondDemoApplication.java
+++ src/main/java/at/adtime/spring/second/demo/SpringSecondDemoApplication.java
@@ -1,12 +1,41 @@
 package at.adtime.spring.second.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Arrays;
 
-@SpringBootApplication
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import at.adtime.spring.second.demo.model.Account;
+import at.adtime.spring.second.demo.model.Bookmark;
+import at.adtime.spring.second.demo.repositories.AccountRepository;
+import at.adtime.spring.second.demo.repositories.BookmarkRepository;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class SpringSecondDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringSecondDemoApplication.class, args);
-    }
+	@Bean
+	CommandLineRunner init(AccountRepository accountRepository,
+			BookmarkRepository bookmarkRepository) {
+		return (evt) -> Arrays.asList(
+				"jhoeller,dsyer,pwebb,ogierke,rwinch,mfisher,mpollack,jlong".split(","))
+				.forEach(
+						a -> {
+							Account account = accountRepository.save(new Account(a,
+									"password"));
+							bookmarkRepository.save(new Bookmark(account,
+									"http://bookmark.com/1/" + a, "A description"));
+							bookmarkRepository.save(new Bookmark(account,
+									"http://bookmark.com/2/" + a, "A description"));
+						});
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecondDemoApplication.class, args);
+	}
 }
