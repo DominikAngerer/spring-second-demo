327120fe373588009aaeffc0ba08ae5e5dea6631
Dominik Angerer <git@adtime.at>
2015-01-22 23:25:39 +0100

    Ups - wrong import - nearly got in some struggles ;)

--- src/main/java/at/adtime/spring/second/demo/repositories/AccountRepository.java
+++ src/main/java/at/adtime/spring/second/demo/repositories/AccountRepository.java
@@ -1,10 +1,11 @@
 package at.adtime.spring.second.demo.repositories;
 
-import org.apache.el.stream.Optional;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import at.adtime.spring.second.demo.model.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional findByUsername(String username);
+    Optional<Account> findByUsername(String username);
 }
