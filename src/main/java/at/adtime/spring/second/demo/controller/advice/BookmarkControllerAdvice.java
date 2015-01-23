ddad19fc191dd96729419caa5d23d9a060220be3
Dominik Angerer <git@adtime.at>
2015-01-23 14:05:14 +0100

    Added BookmarkControllerAdvice

---
+++ src/main/java/at/adtime/spring/second/demo/controller/advice/BookmarkControllerAdvice.java
@@ -0,0 +1,21 @@
+package at.adtime.spring.second.demo.controller.advice;
+
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import at.adtime.spring.second.demo.controller.exception.UserNotFoundException;
+
+@ControllerAdvice
+public class BookmarkControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public VndErrors userNotFoundExceptionHandler(UserNotFoundException ex) {
+        return new VndErrors("error", ex.getMessage());
+    }
+}
