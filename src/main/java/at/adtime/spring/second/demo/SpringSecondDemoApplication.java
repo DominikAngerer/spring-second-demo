0fa8b4442b902d15440acc13b518cedd0508a5d7
Dominik Angerer <git@adtime.at>
2015-01-23 14:23:58 +0100

    Added Cors Filter

--- src/main/java/at/adtime/spring/second/demo/SpringSecondDemoApplication.java
+++ src/main/java/at/adtime/spring/second/demo/SpringSecondDemoApplication.java
@@ -1,13 +1,26 @@
 package at.adtime.spring.second.demo;
 
+import java.io.IOException;
 import java.util.Arrays;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 
 import at.adtime.spring.second.demo.model.Account;
 import at.adtime.spring.second.demo.model.Bookmark;
@@ -34,6 +47,41 @@ public class SpringSecondDemoApplication {
 									"http://bookmark.com/2/" + a, "A description"));
 						});
 	}
+	
+	// CORS
+		@Bean
+		FilterRegistrationBean corsFilter(
+				@Value("${tagit.origin:http://localhost:9000}") String origin) {
+			return new FilterRegistrationBean(new Filter() {
+				public void doFilter(ServletRequest req, ServletResponse res,
+						FilterChain chain) throws IOException, ServletException {
+					HttpServletRequest request = (HttpServletRequest) req;
+					HttpServletResponse response = (HttpServletResponse) res;
+					String method = request.getMethod();
+					// this origin value could just as easily have come from a database
+					response.setHeader("Access-Control-Allow-Origin", origin);
+					response.setHeader("Access-Control-Allow-Methods",
+							"POST,GET,OPTIONS,DELETE");
+					response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
+					response.setHeader("Access-Control-Allow-Credentials", "true");
+					response.setHeader(
+							"Access-Control-Allow-Headers",
+							"Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+					if ("OPTIONS".equals(method)) {
+						response.setStatus(HttpStatus.OK.value());
+					}
+					else {
+						chain.doFilter(req, res);
+					}
+				}
+
+				public void init(FilterConfig filterConfig) {
+				}
+
+				public void destroy() {
+				}
+			});
+		}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecondDemoApplication.class, args);
